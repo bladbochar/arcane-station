724522b53e5a36bf3e9b4875a6ee0fb2d4c481c6
Vecortys <123871947+Vecortys@users.noreply.github.com>
2026-06-15 01:43:34 +0300

    НПС засыпают, если игроки далеко

--- Content.Shared/_Arcane/CCVars/ACCVars.cs
+++ Content.Shared/_Arcane/CCVars/ACCVars.cs
@@ -10,4 +10,10 @@ public sealed partial class ACCVars
     /// </summary>
     public static readonly CVarDef<bool> UseTTS =
         CVarDef.Create("tts.use_tts", true, CVar.CLIENTONLY | CVar.ARCHIVE);
+
+    /// <summary>
+    /// На каком расстоянии от игрока NPC будет замораживаться.
+    /// </summary>
+    public static readonly CVarDef<int> NpcSleepRange =
+        CVarDef.Create("npc.sleep_range", 30, CVar.SERVERONLY);
 }
